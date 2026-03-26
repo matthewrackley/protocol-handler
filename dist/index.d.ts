c26d8471b14fa79715b68d00e79bc3ed4456da76
Matthew Rackley <matthewrackley@MOBILE-DEV.lan>
2026-03-26 13:29:38 -0500

    remove files needed to be ignored

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './factory/createProtocolHandler';
-export type * from './core/ProtocolHandlerCore';
-export type * from './types/index';
-export type * from './processors/HttpProcessor';
-export * from './processors/HttpProcessor';
-export type * from './processors/WsProcessor';
-export * from './types';
-export * from './resolvers/index';
